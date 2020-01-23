2d7c7fdad4b977bc6760d8cc8722b25319c18c86
lobana alarabi <loubanaeng@gmail.com>
2020-01-23 05:33:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Your code goes here
-document.addEventListener("DOMContentLoaded",Function()
-{
+document.addEventListener("DOMContentLoaded",Function(){
   let txt=document.getElementById("text");
   txt.textContent="This is really cool!";
 });
